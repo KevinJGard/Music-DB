7ffaf823a8b47b407711b1f5ec7ab7e559dbe15b
KevinJGard <kevingard@ciencias.unam.mx>
2024-09-26 13:43:43 -0600

    -Hice que el directorio se pase como argumento y si no se intenta correr correctamente muestra el uso correcto y termina el programa.

--- src/main.go
+++ src/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"github.com/KevinJGard/MusicDB/src/model"
 	"fmt"
+	"os"
 	"log"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s <directory>", os.Args[0])
+	}
 	miner := model.NewMiner()
 
-	directory := "/home/kevingardhp/Música"
+	directory := os.Args[1]
 	files, err := miner.FindMP3Files(directory)
 	if err != nil {
 		log.Fatalf("Error traversing directory: %v", err)
