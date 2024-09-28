4e1b5275728d49e57ecf5033d72e848ace3534ce
KevinJGard <kevingard@ciencias.unam.mx>
2024-09-27 19:58:05 -0600

    -Agregue la carpeta de test, con pruebas unitarias para el minero, la primera prueba verifica que se pueda recorrer un directorio y la segunda verifica que se puedan leer las etiquetas y que el archivo tenga etiquetas.

---
+++ src/test/miner_test.go
@@ -0,0 +1,46 @@
+package test
+
+import (
+	"testing"
+	"github.com/KevinJGard/MusicDB/src/model"
+)
+
+func TestFindMP3Files(t *testing.T) {
+	miner := model.NewMiner()
+
+	directory := "/home/kevingardhp/Música/"
+	_, err := miner.FindMP3Files(directory)
+	if err != nil {
+		t.Fatalf("Error traversing directory: %v", err)
+	}
+}
+
+func TestMineMetadata(t *testing.T) {
+	miner := model.NewMiner()
+	file := "/home/kevingardhp/Música/full.mp3"
+	metadata, err := miner.MineMetadata(file)
+	
+	if err != nil {
+		t.Fatalf("Error reading metadata for %s: %v", file, err)
+	}
+
+	if metadata.Title() == "" {
+		t.Error("Tag not found.")
+	}
+	if metadata.Artist() == "" {
+		t.Error("Tag not found.")
+	}
+	if metadata.Album() == "" {
+		t.Error("Tag not found.")
+	}
+	if metadata.Genre() == "" {
+		t.Error("Tag not found.")
+	}
+	if metadata.Year() == 0 {
+		t.Error("Tag not found.")
+	}
+	trackNumber, totalTracks := metadata.Track()
+	if trackNumber == 0 && totalTracks == 0 {
+		t.Error("Tag not found.")
+	}
+}
